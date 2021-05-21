bc53b01d0c98a934c4a5a75c975a30efe2bbb344
Vaibhav verma <vaibhav@iitbhilai.ac.in>
2021-05-21 22:39:50 +0530

    adjacency_matrix

--- Graphs/adjacency_matrix.py
+++ Graphs/adjacency_matrix.py
@@ -1,23 +1,28 @@
 import numpy as np
+
+def create_adjacency_matrix(elist,vlist):
+    matrix=np.zeros((len(vlist),len(vlist)))
+    edges=elist.split(',')
+    for e in edges:
+        w=int(e.split()[1])
+        fr=list(e.split()[0])[0]
+        to=list(e.split()[0])[1]
+        frindex=int(vlist.index(fr))
+        toindex=int(vlist.index(to))
+        matrix[frindex][toindex]=w
+    return matrix
+
 ver= input("list down all vertices of the graph as a string: ")
 vlist=list(ver)
 print("No of vertices in the graph is "+str(len(vlist)))
-print("\nEnter type of graph",end="")
-type=input("d for directed un for undirected: ")
+# print("\nEnter type of graph",end="")
+# type=input("d for directed un for undirected: ")
 print("\nList down edges of the graph")
 print("eg. 12 5,23 2,31 5 and so on..",end="")
 ed=input("where 12 5 shows if there is edge to edge 1 to 2 with edgeweight: ")
-matrix=np.zeros((len(vlist),len(vlist)))
-edges=ed.split(',')
-print("\nNo. of edges  in the graph is "+str(len(edges)))
-for e in edges:
-    w=int(e.split()[1])
-    fr=list(e.split()[0])[0]
-    to=list(e.split()[0])[1]
-    frindex=int(vlist.index(fr))
-    toindex=int(vlist.index(to))
-    matrix[frindex][toindex]=w
-if type =='ud':
-    matrix=np.add(matrix, matrix.transpose())
-print("\nAdjacency matrix: ")
-print(matrix)
+print("No. of edges  in the graph is "+str(len(ed.split(','))))
+
+m=np.zeros((len(vlist),len(vlist)))
+m=create_adjacency_matrix(ed,vlist)
+print("\nAdjacency matrix:")
+print(m)
