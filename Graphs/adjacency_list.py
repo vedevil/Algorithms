a2d28a64f2791480ac9e7f69e70f1aa76a7e2e40
Vaibhav verma <vaibhav@iitbhilai.ac.in>
2021-05-21 22:55:49 +0530

    ad_list

--- Graphs/adjacency_list.py
+++ Graphs/adjacency_list.py
@@ -1,4 +1,20 @@
 import numpy as np
+
+
+def create_adjacency_list(elist,vlist):
+    adli=dict()
+    edges=elist.split(',')
+    for e in edges:
+        w=int(e.split()[1])
+        fr=list(e.split()[0])[0]
+        to=list(e.split()[0])[1]
+        if fr in adli:
+            adli[fr].append(to+str(w))
+        else:
+            adli[fr]=[]
+            adli[fr].append(to+str(w))
+    return adli
+
 ver= input("list down all vertices of the graph as a string: ")
 vlist=list(ver)
 print("No of vertices in the graph is "+str(len(vlist)))
@@ -7,18 +23,9 @@ print("No of vertices in the graph is "+str(len(vlist)))
 print("\nList down edges of the graph")
 print("eg. 12 5,23 2,31 5 and so on..",end="")
 ed=input("where 12 5 shows if there is edge to edge 1 to 2 with edgeweight: ")
-adli=dict()
-matrix=np.zeros((len(vlist),len(vlist)))
-edges=ed.split(',')
-print("\nNo. of edges  in the graph is "+str(len(edges)))
-for e in edges:
-    w=int(e.split()[1])
-    fr=list(e.split()[0])[0]
-    to=list(e.split()[0])[1]
-    if fr in adli:
-        adli[fr].append(to+str(w))
-    else:
-        adli[fr]=[]
-        adli[fr].append(to+str(w))
+print("No. of edges  in the graph is "+str(len(ed.split(','))))
+
+a=dict()
+a=create_adjacency_list(ed, vlist)
 print("\nAdjacency list: ")
-print(adli)
+print(a)
