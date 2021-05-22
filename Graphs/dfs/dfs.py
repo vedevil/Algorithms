83e5007fcda41a1d3fd77ad51388f33f9bffc40d
Vaibhav verma <vaibhav@iitbhilai.ac.in>
2021-05-22 14:01:34 +0530

    added dfs

--- Graphs/dfs/dfs.py
+++ Graphs/dfs/dfs.py
@@ -5,16 +5,17 @@ def dfs(adli,vlist,start_vertex):
     stack=[]
     dfs=[]
     while(len(unvisited)!=0):
-        dfs.append(head)
-        unvisited.remove(head)
-        if head in adli:
-            neighbourww=adli[head]
-            neighbours=[]
-            for n in neighbourww:
-                e=list(n)[0]
-                neighbours.append(e)
-            for e in neighbours:
-                stack.append(e)
+        if head in unvisited:
+            dfs.append(head)
+            unvisited.remove(head)
+            if head in adli:
+                neighbourww=adli[head]
+                neighbours=[]
+                for n in neighbourww:
+                    e=list(n)[0]
+                    neighbours.append(e)
+                for e in neighbours:
+                    stack.append(e)
         if len(unvisited)!=0:
             head=stack.pop()
     return dfs
