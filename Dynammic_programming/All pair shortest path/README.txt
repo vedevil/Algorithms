ca4e2c9eb4d586696b7c40dec242f7ebe7f251c7
Vaibhav verma <vaibhav@iitbhilai.ac.in>
2021-06-08 22:49:02 +0530

    updated All_pair_sp

---
+++ Dynammic_programming/All pair shortest path/README.txt
@@ -0,0 +1,10 @@
+Program is implemented in 3 steps:
+
+Step 1:Taking input as all the vertices(as a string) and all the edges of the graph
+Step 2: Making adjancency matrix with a condition if direct eddge is not present between two vertices, then distance is taken infinity.
+Ste3: Main step of the algo
+We are considering every intermediate vertex and checking if the path via that vertex is etter than direct edge.
+
+Input format
+Line 1 takes vertices of the graph as a  string
+Line 2 takes all the eges of the graph along with  weight.
--- Dynammic_programming/All pair shortest path/apsp.py
+++ Dynammic_programming/All pair shortest path/apsp.py
@@ -1,13 +1,34 @@
+
+import numpy as np
 import sys
 sys.path.append('/home/ved/Documents/Skills/progrmming/Algorithms/Graphs/adjacency_list')
 import adjacency_list_directed as al
 
 with open('input.txt', 'r') as f:
-    ver= f.readline()
+    ver= f.readline().split('\n')[0]
     vlist=list(ver)
     print("No of vertices in the graph is "+str(len(vlist)))
-    ed=f.readline()
+    ed=f.readline().split('\n')[0]
     print("No. of edges  in the graph is "+str(len(ed.split(','))))
-
+inf=99999
+matrix=np.full((len(vlist),len(vlist)),inf)
 a=al.create_adjacency_list(ed, vlist)
-print(a)
+for vertex in a:
+    edges_incident=a[vertex]
+    for e in edges_incident:
+        iv=e[0]
+        w=int(e[1])
+        row_index=vlist.index(vertex)
+        col_index=vlist.index(iv)
+        matrix[row_index][col_index]=w
+for k in vlist:
+    for i in vlist:
+        for j in vlist:
+            k_index=vlist.index(k)
+            i_index=vlist.index(i)
+            j_index=vlist.index(j)
+            if matrix[i_index][k_index]+matrix[k_index][j_index]<matrix[i_index][j_index]:
+                matrix[i_index][j_index]=matrix[i_index][k_index]+matrix[k_index][j_index]  
+for i in range(len(vlist)):
+    matrix[i][i]=0
+print(matrix)
